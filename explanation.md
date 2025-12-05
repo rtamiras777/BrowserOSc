5a57a23ce39c4c58f4e6d4f74010695fea6c3498
Ridhi Tamirasa <ridhi@Mac.attlocal.net>
2025-12-04 16:59:21 -0800

    update

--- explanation.md
+++ explanation.md
@@ -4,7 +4,33 @@ from there click continue
 Then click start cloud onboarding
 Then click add cloud connection
 Prompt user to fill out role name and slect services (with human input tool) then click next
-click download on the generated template file then open a new tab:
+click download on the generated 
+template file 
+then click next
+
+then open a new tab with this:
 https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/template
 
-Then click upload a template file on there and instruct user to upload file
+Then click upload a template file on there and instruct user to upload file just downloaded and click through until completed with AWS steps 
+
+then have a human input tool that clicks yes when the stack is submitted and from there 
+find:Configuration Name *
+
+My AWS Account
+A friendly name for this AWS account configuration
+
+AWS Account ID *
+
+123456789012
+Your 12-digit AWS account ID (e.g., 123456789012)
+
+Role ARN *
+
+arn:aws:iam::123456789012:role/CiroosReadOnlyRole
+The ARN of the IAM role you created in the previous step
+
+Tags (Optional)
+
+Tag Key
+and fill it into the ravi-dev.ciroos previous tab
+
--- packages/browseros-agent/src/sidepanel/components/MessageList.tsx
+++ packages/browseros-agent/src/sidepanel/components/MessageList.tsx
@@ -170,11 +170,21 @@ export function MessageList({ messages, isProcessing = false, onScrollStateChang
 5. IMPORTANT: Ask me (using human input) to fill out the Role Name and select the Services I want
 6. After I provide that information, click "Next"
 7. Download the generated CloudFormation template file
-8. Open a new tab to: https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/template
-9. On the AWS CloudFormation page, click "Upload a template file"
-10. Instruct me to upload the template file you just downloaded
+8. Click "Next" again
+9. Open a new tab to: https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/create/template
+10. On the AWS CloudFormation page, click "Upload a template file"
+11. Instruct me to upload the template file you just downloaded
+12. Guide me through clicking through the AWS CloudFormation steps until completion
+13. IMPORTANT: Use human input to ask me to confirm when the AWS CloudFormation stack has been successfully submitted
+14. After confirmation, go back to the ravi-dev.ciroos.ai tab
+15. Find and fill in the following fields on the page:
+    - Configuration Name: (ask me for a friendly name for this AWS account configuration)
+    - AWS Account ID: (ask me for my 12-digit AWS account ID, e.g., 123456789012)
+    - Role ARN: (ask me for the ARN of the IAM role created, e.g., arn:aws:iam::123456789012:role/CiroosReadOnlyRole)
+    - Tags (Optional): (ask if I want to add any tags)
+16. Submit the configuration form
 
-Please execute this workflow step by step, using visual tools to interact with the pages and asking for my input when needed.`
+Please execute this workflow step by step, using visual tools to interact with the pages and asking for my input when needed. Make sure to keep the ravi-dev.ciroos.ai tab open while working with AWS so you can return to it.`
 
       const msgId = `user_${Date.now()}`
       upsertMessage({ msgId, role: 'user', content: 'Onboard me', ts: Date.now() })
